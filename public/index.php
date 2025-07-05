065ecc8afd689477e3bd0b18c90b0e2eb4051849
xrtvann <irvangoblas@gmail.com>
2025-07-06 01:01:49 +0700

    refactor: update HTML structure and improve CSS theme variables for better styling

--- public/index.php
+++ public/index.php
@@ -4,13 +4,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To-Do List Dashboard</title>
-    <link href="./css/style.css" rel="stylesheet">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="css/style.css">
     <link rel="icon" type="image/png" href="https://img.icons8.com/fluency/96/000000/todo-list.png">
 </head>
 
 <body class="font-poppins">
-    
+    <aside class="h-screen w-72 bg-grey-200 shadow-sm flex flex-col p-4">
+        <div class="brand flex items-center space-x-5 mb-8">
+            <img src="https://img.icons8.com/fluency/96/000000/todo-list.png" alt="todo-list-icon" width="40">
+            <h2 class="text-2xl font-bold text-primary">Todolist App</h2>
+        </div>
+        <nav>
+            <ul class="space-y-4">
+                <li><a href="#" class="block px-4 py-2 rounded  hover:text-white  font-medium bg-primary text-white">Dashboard</a></li>
+                <li><a href="#" class="block px-4 py-2 rounded hover:bg-primary hover:opacity-80 hover:text-white text-gray-700 font-medium">Tasks</a></li>
+                <li><a href="#" class="block px-4 py-2 rounded hover:bg-primary hover:opacity-80     hover:text-white text-gray-700 font-medium">Settings</a></li>
+            </ul>
+        </nav>
+    </aside>
 </body>
 
 </html>
