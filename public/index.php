6332d68d750fb361f8f75058d0cec3adecbae369
xrtvann <irvangoblas@gmail.com>
2025-07-05 15:24:50 +0700

    add initial project files including HTML structure, package configuration, and Tailwind CSS setup

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do List Dashboard</title>
+    <link href="./css/style.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="https://img.icons8.com/fluency/96/000000/todo-list.png">
+</head>
+
+<body class="font-poppins">
+    
+</body>
+
+</html>
