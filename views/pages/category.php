8ab35def04843c3d84016daf373f7eb8f5257aa8
xrtvann <irvangoblas@gmail.com>
2025-07-07 22:33:46 +0700

    add table for view data on category page

--- views/pages/category.php
+++ views/pages/category.php
@@ -9,14 +9,34 @@
         </div>
     </div>
 
-    <table class="w-full bg-white rounded table-auto shadow-sm">
-        <thead>
-            <tr class="bg-gray-200">
-                <th class="py-2 px-4 text-left">#</th>
-                <th class="py-2 px-4 text-left">Name</th>
-                <th class="py-2 px-4 text-left">Created At</th>
-                <th class="py-2 px-4 text-left">Actions</th>
+    <table class="table w-full bg-white rounded shadow-sm">
+        <thead class="table-header-group">
+            <tr class="table-row">
+                <th class="table-cell py-2 px-4 text-left">#</th>
+                <th class="table-cell py-2 px-4 text-left">Name</th>
+                <th class="table-cell py-2 px-4 text-left">Created At</th>
+                <th class="table-cell py-2 px-4 text-left">Actions</th>
             </tr>
         </thead>
+        <tbody class="table-row-group">
+            <tr class="table-row">
+                <td class="table-cell py-2 px-4">1</td>
+                <td class="table-cell py-2 px-4">Work</td>
+                <td class="table-cell py-2 px-4">2023-01-01</td>
+                <td class="table-cell py-2 px-4">
+                    <a href="#" class="text-orange-500"><i class="fas fa-edit"></i></a>
+                    <a href="#" class="text-red-500"><i class="fas fa-trash"></i></a>
+                </td>
+            </tr>
+            <tr class="table-row">
+                <td class="table-cell py-2 px-4">1</td>
+                <td class="table-cell py-2 px-4">Work</td>
+                <td class="table-cell py-2 px-4">2023-01-01</td>
+                <td class="table-cell py-2 px-4">
+                    <a href="#" class="text-orange-500"><i class="fas fa-edit"></i></a>
+                    <a href="#" class="text-red-500"><i class="fas fa-trash"></i></a>
+                </td>
+            </tr>
+        </tbody>
     </table>
 </main>
