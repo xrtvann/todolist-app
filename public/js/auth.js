8d0a5e84fac0905b25ebec09861a15b5f49b7096
xrtvann <irvangoblas@gmail.com>
2025-07-21 17:57:09 +0700

    feat: implement password visibility toggle for sign-in and sign-up forms

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,12 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const togglePassword = document.getElementById("togglePassword");
-  const inputPassword = document.getElementById("inputPassword");
-  const eyeIcon = togglePassword.querySelector("i");
+  const signIn = document.getElementById("signIn");
+  const signUp = document.getElementById("signUp");
 
-  togglePassword.addEventListener("click", function () {
-    const type = inputPassword.getAttribute("type") === "password" ? "text" : "password";
-    inputPassword.setAttribute("type", type);
-    eyeIcon.classList.toggle("fa-eye");
-    eyeIcon.classList.toggle("fa-eye-slash");
-  });
+  if (signIn) {
+    const togglePassword = document.getElementById("toggleSignInPassword");
+    const inputPassword = document.getElementById("inputSignInPassword");
+    const eyeIcon = togglePassword.querySelector("i");
+
+    togglePassword.addEventListener("click", function () {
+      const type =
+        inputPassword.getAttribute("type") === "password" ? "text" : "password";
+      inputPassword.setAttribute("type", type);
+      eyeIcon.classList.toggle("fa-eye");
+      eyeIcon.classList.toggle("fa-eye-slash");
+    });
+  }
+
+  if (signUp) {
+    const togglePassword = document.getElementById("toggleSignUpPassword");
+    const inputPassword = document.getElementById("inputSignUpPassword");
+    const eyeIcon = togglePassword.querySelector("i");
+
+    togglePassword.addEventListener("click", function () {
+      const type =
+        inputPassword.getAttribute("type") === "password" ? "text" : "password";
+      inputPassword.setAttribute("type", type);
+      eyeIcon.classList.toggle("fa-eye");
+      eyeIcon.classList.toggle("fa-eye-slash");
+    });
+
+    const toggleConfirmationPassword = document.getElementById(
+      "toggleConfirmationPassword"
+    );
+    const inputConfirmationPassword = document.getElementById(
+      "inputConfirmationPassword"
+    );
+    const confirmationEyeIcon = toggleConfirmationPassword.querySelector("i");
+
+    toggleConfirmationPassword.addEventListener("click", function () {
+      const type =
+        inputConfirmationPassword.getAttribute("type") === "password"
+          ? "text"
+          : "password";
+      inputConfirmationPassword.setAttribute("type", type);
+      confirmationEyeIcon.classList.toggle("fa-eye");
+      confirmationEyeIcon.classList.toggle("fa-eye-slash");
+    });
+  }
 });
--- public/signup.php
+++ public/signup.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign In</title>
+    <title>Sign Up</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" type="image/png" href="https://img.icons8.com/fluency/96/000000/todo-list.png">
     <link rel="stylesheet" href="css/fontawesome-free/css/all.min.css">
@@ -12,13 +12,23 @@
 
 <body class="bg-gray-100 font-poppins">
     <div class="w-full flex justify-center items-center min-h-screen">
-        <div class="sign-in-container w-110 bg-white shadow-md flex flex-col justify-center py-2 px-6 rounded-xl" id="signIn">
+        <div class="sign-up-container w-110 bg-white shadow-md flex flex-col justify-center py-2 px-6 rounded-xl" id="signUp">
             <div class="header pt-5 flex flex-col items-center">
                 <img src="https://img.icons8.com/fluency/96/000000/todo-list.png" alt="" class="w-13">
-                <h1 class="font-bold text-2xl">Sign In</h1>
+                <h1 class="font-bold text-2xl">Sign Up</h1>
             </div>
             <div class="content my-10 mx-2">
                 <form action="" method="post">
+                    <div
+                        class="input-group flex items-center mb-4 border border-gray-300 focus-within:border-primary rounded-md p-3">
+                        <div class="icon flex justify-center items-center text-gray-600 ms-1 me-2">
+                            <i class="fas fa-id-card"></i>
+                        </div>
+                        <div class="input">
+                            <input type="text" class="focus:outline-none" name="full_name" placeholder="Full Name"
+                                required>
+                        </div>
+                    </div>
                     <div
                         class="input-group flex items-center mb-4 border border-gray-300 focus-within:border-primary rounded-md p-3">
                         <div class="icon flex justify-center items-center text-gray-600 ms-1 me-3">
@@ -35,26 +45,37 @@
                             <i class="fas fa-lock"></i>
                         </div>
                         <div class="input w-full">
-                            <input type="password" id="inputSignInPassword" class="focus:outline-none w-full" name="password" placeholder="Password"
-                                required>
+                            <input type="password" id="inputSignUpPassword" class="focus:outline-none w-full" name="password"
+                                placeholder="Password" required>
                         </div>
                         <div class="show-password">
-                            <div class="icon text-gray-600 cursor-pointer" id="toggleSignInPassword">
+                            <div class="icon text-gray-600 cursor-pointer" id="toggleSignUpPassword">
                                 <i class="fas fa-eye"></i>
                             </div>
                         </div>
                     </div>
-                    <div class="remember-me flex items-center my-5">
-                        <input type="checkbox" id="remember-me" name="remember-me" class="w-4 h-4">
-                        <label for="remember-me" class="ms-2">Remember me</label>
+                    <div
+                        class="input-group flex items-center mb-4 border border-gray-300 focus-within:border-primary rounded-md p-3">
+                        <div class="icon flex justify-center items-center text-gray-600 ms-1 me-3">
+                            <i class="fas fa-lock"></i>
+                        </div>
+                        <div class="input w-full">
+                            <input type="password" id="inputConfirmationPassword" class="focus:outline-none w-full" name="password"
+                                placeholder="Confirm Password" required>
+                        </div>
+                        <div class="show-password">
+                            <div class="icon text-gray-600 cursor-pointer" id="toggleConfirmationPassword">
+                                <i class="fas fa-eye"></i>
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
             <div class="action-button mb-2 mx-2">
-                <p>Don't have an account? <a href="signup.php" class="text-primary">Sign up</a></p>
+                <p>Already have an account? <a href="signin.php" class="text-primary">Sign In</a></p>
                 <button
                     class="w-full py-2 mt-1.5 font-semibold bg-primary opacity-95 hover:opacity-100 text-white rounded-md cursor-pointer">Sign
-                    In</button>
+                    Up</button>
             </div>
         </div>
     </div>
